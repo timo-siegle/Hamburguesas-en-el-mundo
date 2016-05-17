efdfac30492c96afd57da45e28ba51cff8d37102
Timo Siegle <timo.siegle@bechtle.com>
2016-05-18 01:03:22 +0200

    first release

---
+++ Hamburguesas/Hamburguesas/Datos.swift
@@ -0,0 +1,44 @@
+//
+//  Datos.swift
+//  Hamburguesas
+//
+//  Created by Timo Siegle on 16.05.16.
+//  Copyright © 2016 Timo Siegle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColeccionDePaises {
+    let paises: [String] = ["Afganistán", "Alemania", "Argentina", "Australia", "Austria", "Bélgica", "Brasil", "Chile", "Costa Rica", "España", "Estados Unidos", "Francia", "Italia", "Jamaica", "México", "Portugal", "República Dominicana", "Rusia", "Tailandia"]
+    
+    func obtenPais() -> String {
+        let posición = Int(arc4random()) % paises.count
+        return paises[posición]
+    }
+}
+
+class ColeccionDeHamburguesa {
+    
+    // origen: http://www.topito.com/top-menus-mcdonald-insolite-monde
+    let hamburguesas: [String] = ["McPork", "Mega Tomago", "McPaneer", "Les nuggets au brie", "Deluxe Brekkie Roll", "Carbonara Chicken", "German Sausage Chicken", "Samurai Pork Burger", "McKebab", "McRice Burger", "Pasta Zoo", "Laksewrap", "Ebi Filet-O", "McCurry Pan", "Little Chorizo Melt", "Prosperity Burger", "Camembert Premiere", "Gazpacho", "Chocolate and Orange Pie", "McSpaghetti"]
+    
+    func obtenHamburguesa() -> String {
+        let posición = Int(arc4random()) % hamburguesas.count
+        return hamburguesas[posición]
+    }
+}
+
+struct Colores {
+    let colores = [
+        UIColor(red: 30/255, green: 180/255, blue: 24/255, alpha: 1),
+        UIColor(red: 200/255, green: 6/255, blue: 2/255, alpha: 1),
+        UIColor(red: 66/255, green: 55/255, blue: 101/255, alpha: 1),
+        UIColor(red: 158/255, green: 218/255, blue: 222/255, alpha: 1),
+        UIColor(red: 158/255, green: 222/255, blue: 189/255, alpha: 1)]
+    
+    func regresaColor() -> UIColor {
+        let posicion = Int(arc4random()) % colores.count
+        return colores[posicion]
+    }
+}
--- Hamburguesas/Hamburguesas/ViewController.swift
+++ Hamburguesas/Hamburguesas/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Un @IBoutlet para la etiqueta de país.
+    @IBOutlet weak var nombreDePais: UILabel!
+    // Un @IBoutlet para la etiqueta de hamburguesa.
+    @IBOutlet weak var nombreDeLaHamburguesa: UILabel!
+    
+    // Una instancia de la clase ColeccionDePaises
+    let paises = ColeccionDePaises()
+    // Una instancia de la clase ColeccionDeHamburguesas
+    let hamburguesas = ColeccionDeHamburguesa()
+    let colores = Colores()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Un @IBAction para implementar cambiar de país y de hamburguesa
+    @IBAction func darUnaHamburguesa() {
+        // cambia el color de fondo
+        let color = colores.regresaColor()
+        view.backgroundColor = color
+        view.tintColor = color
+        
+        // cambiar el país y la hamburguesa que se despliegan en las etiquetas
+        nombreDePais.text = paises.obtenPais()
+        nombreDeLaHamburguesa.text = hamburguesas.obtenHamburguesa()
+    }
 
 }
 
